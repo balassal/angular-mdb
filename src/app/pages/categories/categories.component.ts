8a41530f06d353553d3d3ee4202fd17c56ed797f
BLacika <b.lacika23@gmail.com>
2020-07-23 13:10:41 +0200

    Added Category model

--- src/app/pages/categories/categories.component.ts
+++ src/app/pages/categories/categories.component.ts
@@ -1,14 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { CategoryService } from '../../services/category.service';
 
 @Component({
   templateUrl: './categories.component.html',
-  styleUrls: ['./categories.component.scss']
+  styleUrls: ['./categories.component.scss'],
 })
 export class CategoriesComponent implements OnInit {
+  categories: Observable<Category[]>;
 
-  constructor() { }
+  constructor(private categoryService: CategoryService) {}
 
   ngOnInit(): void {
+    this.loadData();
   }
 
+  loadData() {
+    this.categories = this.categoryService.getAllCategory();
+  }
 }
