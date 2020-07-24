01909e5b472da554a0ac898f70ffbdf3d6b4ad6b
BLacika <b.lacika23@gmail.com>
2020-07-24 16:31:58 +0200

    Category Done

--- src/app/pages/categories/categories.component.ts
+++ src/app/pages/categories/categories.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { CategoryService } from '../../services/category.service';
+import { Category } from '../../models/category.model';
 
 @Component({
   templateUrl: './categories.component.html',
@@ -8,6 +9,9 @@ import { CategoryService } from '../../services/category.service';
 })
 export class CategoriesComponent implements OnInit {
   categories: Observable<Category[]>;
+  categoryTitle: string = '';
+  categoryId: number;
+  willUpdate = false;
 
   constructor(private categoryService: CategoryService) {}
 
@@ -16,6 +20,53 @@ export class CategoriesComponent implements OnInit {
   }
 
   loadData() {
-    this.categories = this.categoryService.getAllCategory();
+    this.categoryService.getAllCategory().subscribe(
+      (data) => {
+        this.categories = data;
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+
+  createCategory() {
+    let category = new Category();
+    category.title = this.categoryTitle;
+    this.categoryService.createCategory(category).subscribe(
+      (data) => console.log(data),
+      (error) => console.log(error)
+    );
+    this.categoryTitle = '';
+    this.loadData();
+  }
+
+  updateCategory() {
+    let category = new Category();
+    category.id = this.categoryId;
+    category.title = this.categoryTitle;
+    this.categoryService.updateCategory(this.categoryId, category).subscribe(
+      (data) => console.log(data),
+      (error) => console.log(error)
+    );
+    this.categoryTitle = '';
+    this.categoryId = null;
+    this.willUpdate = false;
+    this.loadData();
+  }
+
+  onDelete(id: number) {
+    this.categoryService.deleteCategory(id).subscribe(
+      (data) => {
+        console.log(data);
+        this.loadData();
+      },
+      (error) => console.log(error)
+    );
+  }
+
+  onEdit(id: number) {
+    this.willUpdate = true;
+    this.categoryId = id;
   }
 }
--- src/app/services/category.service.ts
+++ src/app/services/category.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Category } from '../models/category.model';
 
 @Injectable({
   providedIn: 'root',
@@ -9,7 +11,23 @@ export class CategoryService {
 
   constructor(private http: HttpClient) {}
 
-  getAllCategory() {
+  getAllCategory(): Observable<any> {
     return this.http.get(`${this.baseUrl}`);
+  }
+
+  getCategoryById(id: number): Observable<any> {
+    return this.http.get(`${this.baseUrl}/${id}`);
+  }
+
+  createCategory(category: Object): Observable<Object> {
+    return this.http.post(`${this.baseUrl}/create`, category);
+  }
+
+  deleteCategory(id: number): Observable<any> {
+    return this.http.delete(`${this.baseUrl}/${id}`, { responseType: 'text' });
+  }
+
+  updateCategory(id: number, value: any): Observable<Object> {
+    return this.http.put(`${this.baseUrl}/${id}`, value);
   }
 }
