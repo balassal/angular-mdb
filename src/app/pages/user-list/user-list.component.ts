6c0ef312198e38bf056a60bd3bf8a41196960245
BLacika <b.lacika23@gmail.com>
2020-08-04 10:35:56 +0200

    Added user list, and search field

--- src/app/pages/user-list/user-list.component.ts
+++ src/app/pages/user-list/user-list.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 import { User } from '../../models/user.model';
 import { UserService } from '../../services/user.service';
+import { Router } from '@angular/router';
 
 @Component({
   templateUrl: './user-list.component.html',
@@ -8,13 +9,51 @@ import { UserService } from '../../services/user.service';
 })
 export class UserListComponent implements OnInit {
   users: User[];
+  searchText: string = '';
 
-  constructor(private userService: UserService) {}
+  constructor(private userService: UserService, private router: Router) {}
 
-  ngOnInit(): void {
+  @HostListener('input') oninput() {
+    this.searchItems();
+  }
+
+  ngOnInit() {
+    this.loadData();
+  }
+
+  loadData() {
     this.userService.getAllUser().subscribe(
       (data) => (this.users = data),
       (error) => console.log(error)
     );
+  }
+
+  deleteUser(id: number) {
+    if (confirm('Are u sure to delete this USER?')) {
+      this.users = this.users.filter((user) => user.id !== id);
+      this.userService.deleteUser(id).subscribe(
+        (data) => console.log(data),
+        (error) => console.log(error)
+      );
+    }
+  }
+
+  onAddUser() {
+    this.router.navigate(['posts/add']);
+  }
+
+  searchItems() {
+    if (!this.searchText) {
+      this.loadData();
+    } else {
+      this.users = this.users.filter((user) => {
+        return (
+          user.username.includes(this.searchText) ||
+          user.id.toString().includes(this.searchText) ||
+          user.email.includes(this.searchText)
+        );
+      });
+      console.log(this.users);
+    }
   }
 }
